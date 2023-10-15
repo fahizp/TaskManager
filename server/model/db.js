e151fa543d7deac45c1d639e1df4e08daf03e0ff
fahizp <fahizp3@gmail.com>
2023-10-15 09:53:28 +0530

    fix: the db connection problem

--- server/model/db.js
+++ server/model/db.js
@@ -1,18 +1,19 @@
 import mysql from "mysql";
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 export const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Fahiz@2003",
-  database: "task"
+  password:process.env.DB_KEY,
+  database: "task",
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to database');
+    console.error("Error connecting to database");
     process.exit(1);
   }
-  console.log('Connected to database');
+  console.log("Connected to database");
 });
 
 export default db;
